e69579f00c929c3299f36783c1ff9ff1ea0a14b7
Juan Gaitán-Villamizar <juans.gaitan@gmail.com>
2024-09-07 15:35:19 +0200

    Update about me description

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">About me</h1>
       <p className="mb-4 text-justify text-md">
         {
-          "I'm Juan, a software engineer obsessed with the art of building. My path weaves through web, data, and security—each a different lens on the same core challenge: how to create value in the digital age. Nowadays, I'm pushing the boundaries of AI at "
+          'I’m Juan, a software engineer with a deep interest in the craft of building. My journey has taken me through web, data, and security—each offering a unique perspective on the shared challenge of creating value in the digital world. These days, I’m exploring the evolving field of AI/ML at '
         }
         <Link href="https://www.epilot.cloud/en" className="text-blue-500">
           epilot
         </Link>
         {
-          ". Beyond code, I'm a father and husband, finding parallels between nurturing a family and nurturing technology. Both require patience, creativity, and a willingness to embrace the unknown."
+          ', learning more every day. Outside of work, I’m a father and husband, and I’ve found surprising connections between raising a family and building technology products. Both demand patience, creativity, and an openness to uncertainty, which keeps me curious and always eager to grow.'
         }
       </p>
       <div className="my-8 self-start">
