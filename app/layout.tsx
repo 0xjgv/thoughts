17012c887713f536e91dde4d4441127abbee9312
Juan Gaitán-Villamizar <juans.gaitan@gmail.com>
2025-12-16 12:27:08 +0100

    Update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
+import type { Metadata } from 'next'
 import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import { Navbar } from './components/nav'
 import { ThemeProvider } from './components/theme-provider'
+import './global.css'
 import { getTheme } from './lib/theme'
+import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
     template: '%s | 0xjgv',
     default: '0xjgv'
   },
-  description: 'About me & some thoughts.',
+  description: 'My corner of the internet where I overthink things in public.',
   openGraph: {
-    description: 'About me & some thoughts.',
+    description: 'My corner of the internet where I overthink things in public.',
     siteName: '0xjgv',
     title: '0xjgv',
     locale: 'en_US',
